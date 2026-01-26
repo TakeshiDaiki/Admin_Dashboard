bf197ea624fcf3ffed6c48230b7ad55d1d8b8a40
José Salazar <josesaladiaz08@gmail.com>
2026-01-25 21:53:46 -0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-  experimental: {
-    appDir: True
-  }
+ 
 };
 
 export default nextConfig;
